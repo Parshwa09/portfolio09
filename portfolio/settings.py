06bd0d426e322dd11026fd9b43ec9f803faf856f
Parshwa09 <144384578+Parshwa09@users.noreply.github.com>
2025-09-12 10:31:04 +0530

    Fix sympy version conflict with torch 2.6.0

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = '_8y_ouszs*l&bxcw@+!ybx_^##tb=29(s@ak6l%5#(go!=u!in'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["portfolio09-production.up.railway.app"]
+ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -88,7 +88,6 @@ DATABASES = {
         'PORT': '51942',
     }
 }
-
 
 
 # Password validation
